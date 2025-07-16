c1979e90e7b81f514a7f1f2aaeb1bbb5fb536529
ARCHIVE <archive>
2025-07-17 00:35:07 +0100

    new updates

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket 
 import struct 
-# import time 
 from utils.utils import file_handler, create_client
 
 # HOST = socket.gethostbyname(socket.gethostname())
@@ -64,38 +63,6 @@ def test_ack_header():
 # test_ack_header()
 
 
-# def stream_file(fname: str) -> None:
-#     s = create_client(ADDR)
-#     file = file_handler(fname)
-#     content = file.readlines()
-#
-#     codec_author = fname.encode(FORMAT)
-#     codec_content = [line.encode(FORMAT) for line in content]
-#     N_PACKETS = len(codec_content)
-#     print("total-packets to send: ", N_PACKETS)
-#
-#     # if the server did not send an Acknowledged status we abort the mission for now
-#     status = send_ack(s, N_PACKETS, codec_author)
-#     if not status:
-#         exit()
-#
-#     packet_sync = 0
-#     enc_type = struct.pack("B", len(TYPE_DATA_TRANS))
-#     while packet_sync < N_PACKETS:
-#         body = enc_type + TYPE_DATA_TRANS + codec_content[packet_sync]
-#         header = struct.pack("!I", len(body))
-#
-#         packet = header + body
-#         # time.sleep(0.5)
-#         s.sendall(packet)
-#
-#         packet_sync += 1
-#         # print(f"packet-sync: {packet_sync} -> {codec_content[packet_sync]}")
-#
-#     print("done")
-#     s.close()
-
-
 def streamer(fname: str) -> None:
     print(f"streaming {fname}")
     s = create_client(ADDR)
@@ -121,7 +88,7 @@ def streamer(fname: str) -> None:
     print("total packets to send:", N_PACKETS)
 
     status = send_ack(s, N_PACKETS, fname.encode(FORMAT))
-    
+
     if not status:
         print("Server did not acknowledge request")
         s.close()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys 
+import os 
 from client import streamer
 
 
@@ -12,4 +13,13 @@ def read_args() -> None:
         streamer(file)
 
 
-read_args()
+# read_args()
+# def mkdirs(path_name: str) -> None:
+#     isFolder = path_name.contains("/")
+#     file_name = path_name
+#     folder_name = ""
+#     if isFolder:
+#         folder_stuct = path_name.split("/")
+#         file_name = folder_struct[1]
+#         folder_name = folder_struct[0]
+#     pass
