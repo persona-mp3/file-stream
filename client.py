c20316e270bc9b298ebb7976d4e3fa8b04564aed
ARCHIVE <archive>
2025-07-18 15:07:22 +0100

    basic functionality working

--- client.py
+++ client.py
@@ -11,6 +11,8 @@ PORT = 6000
 ADDR = (HOST, PORT)
 
 ACK_REQ = "Acknowledge \r\n"
+PANIC_REQ = "Panic \r\n"
+
 ACK_RES = "Acknowledged"
 DATA_PACKET = "Packet".encode("utf-8")
 
@@ -63,4 +65,80 @@ def test_ack_res():
     return 
 
 
-test_ack_res()
+# test_ack_res()
+
+
+def streamer(file_name: str) -> None:
+    s = create_client(ADDR)
+    req_type = DATA_PACKET
+    req_len = struct.pack("B", len(req_type))
+
+    file = file_handler(file_name, "rb")
+    packet_sync = 0
+
+    CHUNK_SIZE = 1024
+    chunks = []
+
+    while True:
+        chunk = file.read(CHUNK_SIZE)
+        if not chunk:
+            print("EOF file reached, nothing more to read")
+            file.close()
+            break
+
+        chunks.append(chunk)
+
+    # represents the number of packets the server should expect
+    file.close()
+    N_PACKETS = len(chunks)
+    print(f"Total packets to send to server: {N_PACKETS}")
+
+    print("\n === Waiting to get Acked === \n")
+    is_acked = get_acked(s, N_PACKETS, file_name)
+    if not is_acked:
+        s.close()
+        exit()
+
+    print("\n === preparing packets to send === \n")
+
+    while packet_sync < N_PACKETS:
+        tag = str(packet_sync).encode(FORMAT)
+        sent = str(packet_sync).encode(FORMAT)
+
+        enc_tag_len = struct.pack("!I", len(tag))
+        enc_sent_len = struct.pack("!I", len(sent))
+
+        payload = req_len + req_type + enc_sent_len + sent + enc_tag_len + tag + chunks[packet_sync]
+        header = struct.pack("!I", len(payload))
+
+        packet = header + payload
+        s.sendall(packet)
+
+        packet_sync += 1
+
+    print(" === All packets sent, closing connection ===")
+    s.close()
+
+
+def read_args() -> None:
+    """
+    Accepts Commnadline argumnets as the files to streamed over to the server
+    If any file does not exists we Abort/Panic
+    """
+    args = sys.argv
+    if len(args) < 2:
+        print("fatal: not enough arguments passed in")
+        exit()
+
+    files = args[1:]
+
+    for file in files:
+        if not os.path.exists(file):
+            print("fatal: this file does not exist")
+            exit()
+        else:
+            print("streaming: ", file)
+            streamer(file)
+
+
+read_args()
